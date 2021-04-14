59aeaeacce74afb2fc2d89bc68b318979415c7b8
ZhimengChen <58173996@qq.com>
2021-04-14 22:46:59 +0800

    Update f2b_regression.py

--- scripts/airflow/regression/f2b_regression.py
+++ scripts/airflow/regression/f2b_regression.py
@@ -90,11 +90,11 @@ if __name__ == '__main__':
         RunEodPositionDefaultClosePdTest(current_date),  # 18. merge position and risk
         RunEodMarketRiskByBookUnderlyerDefaultClosePdTest(current_date),  # 19
         # RunEodSpotScenariosByMarketDefaultClosePdTest(current_date),  # 20
-        RunEodCounterPartyMarketRiskByUnderlyerDefaultClosePdTest(current_date),  # 21. 交易对手分品种风险报告
-        RunEodCounterpartyMarketRiskDefaultCloseDdTest(current_date),  # 22 交易对手风险报告
-        RunEodSubsidiaryMarketRiskDefaultClosePdTest(current_date),  # 23. 各子公司整体风险报告
-        RunEodMarketRiskSummaryDefaultClosePdTest(current_date),  # 24. 全市场整体风险汇总报告
-        RunEodMarketRiskDetailDefaultClosePdTest(current_date)  # 25. 全市场分品种风险报告
+        # RunEodCounterPartyMarketRiskByUnderlyerDefaultClosePdTest(current_date),  # 21. 交易对手分品种风险报告
+        # RunEodCounterpartyMarketRiskDefaultCloseDdTest(current_date),  # 22 交易对手风险报告
+        # RunEodSubsidiaryMarketRiskDefaultClosePdTest(current_date),  # 23. 各子公司整体风险报告
+        # RunEodMarketRiskSummaryDefaultClosePdTest(current_date),  # 24. 全市场整体风险汇总报告
+        # RunEodMarketRiskDetailDefaultClosePdTest(current_date)  # 25. 全市场分品种风险报告
     ]
     for test_case in test_suite:
         print(type(test_case))
